09f080d627894b9d8598ff6d13733308f8349db8
NichiNect <yoniwidhi2@gmail.com>
2020-12-15 09:16:38 +0700

    config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAGrUDSj3yXIaKcoS8-yoxGIMyLp9sAu3Y",
+    authDomain: "az-clone-challange-cca87.firebaseapp.com",
+    projectId: "az-clone-challange-cca87",
+    storageBucket: "az-clone-challange-cca87.appspot.com",
+    messagingSenderId: "886138706234",
+    appId: "1:886138706234:web:0f3463e4cfc961f7795803",
+    measurementId: "G-7T12RE9Y4C"
+};
