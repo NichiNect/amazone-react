ed40dbc963776eb43fac2fc9532ed3dba4b77120
NichiNect <yoniwidhi2@gmail.com>
2020-12-22 09:15:16 +0700

    add to basket with react the data layer, dynamic icon basket

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header(props) {
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className="header">
             <Link to="/">
@@ -38,7 +40,7 @@ function Header(props) {
                     <div className="header_optionBasket">
                         <ShoppingBasketIcon />
                         <span className="header_optionLine2 header_basketCount">
-                            0
+                            {basket?.length}
                         </span>
                     </div>
                 </Link>
